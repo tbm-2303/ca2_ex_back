c752a9398c7ecf75230e24ff1c1081b285f4fa29
timmy <kris>
2022-04-22 05:49:51 +0200

    .

---
+++ src/main/java/rest/DemoResource.java
@@ -0,0 +1,185 @@
+package rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import entities.Role;
+import entities.User;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.Scanner;
+import javax.annotation.security.RolesAllowed;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+import javax.ws.rs.core.*;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import utils.EMF_Creator;
+import utils.HttpUtils;
+
+/**
+ * @author lam@cphbusiness.dk
+ */
+@Path("info")
+public class DemoResource {
+    
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    @Context
+    private UriInfo context;
+
+    @Context
+    SecurityContext securityContext;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getInfoForAll() {
+        return "{\"msg\":\"Hello anonymous\"}";
+    }
+
+    //Just to verify if the database is setup
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("all")
+    public String allUsers() {
+
+        EntityManager em = EMF.createEntityManager();
+        try {
+            TypedQuery<User> query = em.createQuery ("select u from User u",entities.User.class);
+            List<User> users = query.getResultList();
+            return "[" + users.size() + "]";
+        } finally {
+            em.close();
+        }
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("setup")
+    public String setup(){
+
+        EntityManager em = EMF.createEntityManager();
+
+        // IMPORTAAAAAAAAAANT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        // This breaks one of the MOST fundamental security rules in that it ships with default users and passwords
+        // CHANGE the three passwords below, before you uncomment and execute the code below
+        // Also, either delete this file, when users are created or rename and add to .gitignore
+        // Whatever you do DO NOT COMMIT and PUSH with the real passwords
+
+        User user = new User("timmy", "timmy123");
+        User admin = new User("james", "james123");
+        User both = new User("kent", "kent123");
+
+        if(admin.getUserPass().equals("test")||user.getUserPass().equals("test")||both.getUserPass().equals("test"))
+            throw new UnsupportedOperationException("You have not changed the passwords");
+
+        em.getTransaction().begin();
+        Role userRole = new Role("user");
+        Role adminRole = new Role("admin");
+        user.addRole(userRole);
+        admin.addRole(adminRole);
+        both.addRole(userRole);
+        both.addRole(adminRole);
+        em.persist(userRole);
+        em.persist(adminRole);
+        em.persist(user);
+        em.persist(admin);
+        em.persist(both);
+        em.getTransaction().commit();
+        System.out.println("PW: " + user.getUserPass());
+        System.out.println("Testing user with OK password: " + user.verifyPassword("timmy123"));
+        System.out.println("Testing user with wrong password: " + user.verifyPassword("test1"));
+        System.out.println("Created TEST Users");
+        return "{\"msg\":\"setup all good\"}";
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("randomjoke")
+    public String getRandomJoke() throws IOException {
+        String joke = HttpUtils.fetchData("https://api.chucknorris.io/jokes/random");
+        return "{\"msg\": \"Random Joke: " + joke + "\"}";
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("catfact")
+    public String catFact() throws IOException {
+        String fact = HttpUtils.fetchData("https://catfact.ninja/fact");
+        return "{\"msg\": \"cat fact: " + fact + "\"}";
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("catfactx")
+    public Response test() throws IOException {
+       return Response.ok().entity(GSON.toJson(getCatFact("https://catfact.ninja/fact"))).build();
+    }
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("randomjokex")
+    public Response test2() throws IOException {
+        return Response.ok().entity(GSON.toJson(getCatFact("https://api.chucknorris.io/jokes/random"))).build();
+    }
+//d
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("user")
+    @RolesAllowed("user")
+    public String getFromUser() {
+        String thisuser = securityContext.getUserPrincipal().getName();
+        return "{\"msg\": \"Hello to User: " + thisuser + "\"}";
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("admin")
+    @RolesAllowed("admin")
+    public String getFromAdmin() {
+        String thisuser = securityContext.getUserPrincipal().getName();
+        return "{\"msg\": \"Hello to (admin) User: " + thisuser + "\"}";
+    }
+
+
+
+
+
+
+
+
+
+
+    public JsonObject getCatFact(String urls) {
+        try {
+            URL url = new URL(urls);//your url i.e fetch data from .
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("User-Agent", "server");
+            conn.setRequestProperty("Accept", "application/json;charset=UTF-8");
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed: HTTP Error code : " + conn.getResponseCode());
+            }
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+            BufferedReader br = new BufferedReader(in);
+            String output = br.readLine();
+            JsonObject object = new Gson().fromJson(output, JsonObject.class);
+            conn.disconnect();
+            return object;
+
+        } catch (Exception e) {
+            System.out.println("Exception in NetClientGet:- " + e);
+            JsonObject error = new Gson().fromJson(new Gson().toJson(e), JsonObject.class);
+            return error;
+        }
+    }
+}
